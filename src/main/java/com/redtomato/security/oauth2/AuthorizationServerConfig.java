48349ca72aca1bef232a2578223d4c41154ec019
liujingming <redtomatoL@163.com>
2019-11-28 07:15:06 +0800

    oauth

---
+++ src/main/java/com/redtomato/security/oauth2/AuthorizationServerConfig.java
@@ -0,0 +1,14 @@
+package com.redtomato.security.oauth2;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+/**
+ * @author ljm
+ * @version V1.0
+ * @date 2019/11/28
+ **/
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig {
+}
